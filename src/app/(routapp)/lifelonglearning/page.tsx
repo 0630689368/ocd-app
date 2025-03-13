35b723a9bad45ef4df775e401d66548dd5bf4382
“Prapassondsj” <ppsdonsomjit@gmail.com>
2025-03-13 20:01:23 +0700

    first commit

---
+++ src/app/(routapp)/lifelonglearning/page.tsx
@@ -0,0 +1,10 @@
+import Lifeionglearning from "@/components/Lifeionglearning";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Lifeionglearning />
+    </div>
+  );
+}
